b8aeba67570549be07d76f4b4da09f1732f5d4ec
Gururaj254 <gururajdharmashetti390@gmail.com>
2025-12-29 16:30:56 +0530

    Jwt Based spring security implemented

--- src/main/java/com/Guru/BankingApp/mapper/AccountMapper.java
+++ src/main/java/com/Guru/BankingApp/mapper/AccountMapper.java
@@ -5,10 +5,12 @@ import com.Guru.BankingApp.entity.Account;
 
 public class AccountMapper {
 
+    //accountDto -> account
     public static Account mapToAccount(AccountDto dto) {
         return new Account(dto.getId(), dto.getAccountHolderName(), dto.getBalance());
     }
 
+    //account -> accountDto
     public static AccountDto mapToAccountDto(Account account) {
         return new AccountDto(account.getId(), account.getAccountHolderName(), account.getBalance());
     }
